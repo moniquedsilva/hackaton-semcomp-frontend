8bb46ae8caedae89708fa8aae9eb0c7f7abe93b2
Elis Alcantara <emarcelatfs@gmail.com>
2022-09-03 00:37:40 -0300

    wip

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -7,5 +7,11 @@ type StyledButtonProps = {
 export const Button = styled.button<StyledButtonProps>`
   height: 48px;
   width: 100%;
+  background-color: #1e40af;
   max-width: ${({ maxWidth }) => `${maxWidth}px`};
+
+  span {
+    font-size: 16px;
+    color: white;
+  }
 `;
