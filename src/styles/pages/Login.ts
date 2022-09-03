4cf33fc3874faeb669f55dc4e0d6a93ad23bebb4
Elis Alcantara <emarcelatfs@gmail.com>
2022-09-03 02:45:55 -0300

    fix

--- src/styles/pages/Login.ts
+++ src/styles/pages/Login.ts
@@ -7,10 +7,18 @@ export const LoginContainer = styled.div`
 
 export const BasicContainer = styled.div`
   width: 100%;
+
+  &:nth-child(1) {
+    @media (max-width: 768px) {
+      display: none;
+    }
+  }
 `;
 
 export const Circle = styled.div`
   position: absolute;
+  width: 50%;
+  height: 100%;
   background: #60a5fa;
   border-radius: 0px 0px 5000px 0px;
 `;
